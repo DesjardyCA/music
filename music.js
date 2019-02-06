401d1571fc314e6708a56c6bdaa4a2bcbc390be1
Yves Desjardins <YvesDesjardins@gmail.com>
2019-02-06 13:03:46 -0800

    finalized printPlaylist function

--- music.js
+++ music.js
@@ -1,36 +1,36 @@
 var library = {
-    tracks: {
-        t01: {
-            id: "t01",
-            name: "Code Monkey",
-            artist: "Jonathan Coulton",
-            album: "Thing a Week Three"
-        },
-        t02: {
-            id: "t02",
-            name: "Model View Controller",
-            artist: "James Dempsey",
-            album: "WWDC 2003"
-        },
-        t03: {
-            id: "t03",
-            name: "Four Thirty-Three",
-            artist: "John Cage",
-            album: "Woodstock 1952"
-        }
+  tracks: {
+    t01: {
+      id: "t01",
+      name: "Code Monkey",
+      artist: "Jonathan Coulton",
+      album: "Thing a Week Three"
     },
-    playlists: {
-        p01: {
-            id: "p01",
-            name: "Coding Music",
-            tracks: ["t01", "t02"]
-        },
-        p02: {
-            id: "p02",
-            name: "Other Playlist",
-            tracks: ["t03"]
-        }
+    t02: {
+      id: "t02",
+      name: "Model View Controller",
+      artist: "James Dempsey",
+      album: "WWDC 2003"
+    },
+    t03: {
+      id: "t03",
+      name: "Four Thirty-Three",
+      artist: "John Cage",
+      album: "Woodstock 1952"
     }
+  },
+  playlists: {
+    p01: {
+      id: "p01",
+      name: "Coding Music",
+      tracks: ["t01", "t02"]
+    },
+    p02: {
+      id: "p02",
+      name: "Other Playlist",
+      tracks: ["t03"]
+    }
+  }
 }
 
 // FUNCTIONS TO IMPLEMENT:
@@ -40,13 +40,13 @@ var library = {
 // p02: Other Playlist - 1 tracks
 
 var printPlaylists = function () {
-    for (const passthrough in library.playlists) {
-        list = library.playlists[passthrough];
-        console.log(list.id, ":", list.name, "-", list.tracks.length, "tracks");
-    }
+  for (const passthrough in library.playlists) {
+    list = library.playlists[passthrough];
+    console.log(list.id, ":", list.name, "-", list.tracks.length, "tracks");
+  }
 }
 
-printPlaylists();
+// printPlaylists();
 
 
 // prints a list of all tracks, in the form:
@@ -55,14 +55,14 @@ printPlaylists();
 // t03: Four Thirty-Three by John Cage (Woodstock 1952)
 
 var printTracks = function () {
-    for (const passthrough in library.tracks) {
-        track = library.tracks[passthrough];
+  for (const passthrough in library.tracks) {
+    track = library.tracks[passthrough];
 
-        console.log(track.id, ":", track.name, "by", track.artist, "(", track.album, ")");
-    }
+    console.log(track.id, ":", track.name, "by", track.artist, "(", track.album, ")");
+  }
 }
 
-printTracks();
+// printTracks();
 
 
 // prints a list of tracks for a given playlist, in the form:
@@ -70,8 +70,17 @@ printTracks();
 // t01: Code Monkey by Jonathan Coulton (Thing a Week Three)
 // t02: Model View Controller by James Dempsey (WWDC 2003)
 
-var printPlaylist = function (playlistId) {}
+var printPlaylist = function (playlistId) {
+  list = library.playlists[playlistId];
+  console.log(list.id, ":", list.name, "-", list.tracks.length, "tracks");
+  for (const passthrough in list.tracks) {
+    track = library.tracks[list.tracks[passthrough]];
 
+    console.log(track.id, ":", track.name, "by", track.artist, "(", track.album, ")");
+  }
+}
+
+printPlaylist("p01");
 
 // adds an existing track to an existing playlist
 
@@ -79,10 +88,10 @@ var addTrackToPlaylist = function (trackId, playlistId) {}
 
 
 // generates a unique id
-// (use this for addTrack and addPlaylist)
+// (use this for addTrack and addPlaylist) 
 
 var uid = function () {
-    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
 }
 
 
